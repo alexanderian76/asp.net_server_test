f9dc5b3f0bfe8622a04328fba84206983769d45c
Alexander <alexanderian@MacBook-Pro-Alexander.local>
2022-11-24 22:28:07 +0300

    Pdf added

--- TestServer/Controllers/HomeController.cs
+++ TestServer/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using TestServer.Models;
 using Microsoft.EntityFrameworkCore;
+using iTextSharp.text.pdf;
+using iTextSharp;
+using iTextSharp.text;
 
 namespace TestServer.Controllers;
 
@@ -25,6 +28,69 @@ public class HomeController : Controller
 	{
 		return View();
 	}
+
+	public async Task<IActionResult> GetPdfFromHtml()
+	{
+        Byte[] bytes;
+
+        //Boilerplate iTextSharp setup here
+        //Create a stream that we can write to, in this case a MemoryStream
+        using (var ms = new MemoryStream())
+        {
+
+            //Create an iTextSharp Document which is an abstraction of a PDF but **NOT** a PDF
+            using (var doc = new Document())
+            {
+
+                //Create a writer that's bound to our PDF abstraction and our stream
+                using (var writer = PdfWriter.GetInstance(doc, ms))
+                {
+
+                    //Open the document for writing
+                    doc.Open();
+
+                    //Our sample HTML and CSS
+                    var example_html = @"<p>This <em>is </em><span class=""headline"" style=""text-decoration: underline;"">some</span> <strong>sample <em> text</em></strong><span style=""color: red;"">!!!</span></p>";
+                    var example_css = @".headline{font-size:200%}";
+
+                    /**************************************************
+                     * Example #1                                     *
+                     *                                                *
+                     * Use the built-in HTMLWorker to parse the HTML. *
+                     * Only inline CSS is supported.                  *
+                     * ************************************************/
+
+                    //Create a new HTMLWorker bound to our document
+                    using (var htmlWorker = new iTextSharp.text.html.simpleparser.HTMLWorker(doc))
+                    {
+
+                        //HTMLWorker doesn't read a string directly but instead needs a TextReader (which StringReader subclasses)
+                        using (var sr = new StringReader(example_html))
+                        {
+
+                            //Parse the HTML
+                            htmlWorker.Parse(sr);
+                        }
+                    }
+                    doc.Close();
+                }
+            }
+
+            //After all of the PDF "stuff" above is done and closed but **before** we
+            //close the MemoryStream, grab all of the active bytes from the stream
+            bytes = ms.ToArray();
+        }
+
+		//Now we just need to do something with those bytes.
+		//Here I'm writing them to disk but if you were in ASP.Net you might Response.BinaryWrite() them.
+		//You could also write the bytes to a database in a varbinary() column (but please don't) or you
+		//could pass them to another function for further PDF processing.
+		var testFile = "text.pdf";
+			//Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "test.pdf");
+        System.IO.File.WriteAllBytes(testFile, bytes);
+		var file = System.IO.File.Open(testFile, FileMode.Open);
+		return Ok(file) ;
+    }
 
 	public async Task<IActionResult> Get()
 	{
--- TestServer/wwwroot/js/site.js
+++ TestServer/wwwroot/js/site.js
@@ -6,8 +6,28 @@ function getPhoneById() {
     console.log($("#input_id").val())
     fetch("Home/GetPhoneById?id=" + $("#input_id").val()).then(res => res.text().then(txt => $("#title").html(txt)))
 }
+
+
+
+
 function getTestPdf() {
-    fetch("Home/GetTestPdf");
+    let data = []
+    
+    fetch("Home/GetPdfFromHtml").then(res => {
+        const reader = res.body.getReader()
+        function readStream({ done, value }) {
+            if (done) {
+                saveByteArray("name.pdf", data)
+                return;
+            }
+            data.push(value)
+            return reader.read().then(readStream);
+        }
+        reader.read().then(d => {
+            readStream(d)
+        })
+        
+    });
 }
 
 function inputChanged(e) {
@@ -17,6 +37,16 @@ function inputChanged(e) {
 function createPhoneWithRandomPrice() {
     fetch("Home/CreateRandomPhone?title=" + $("#input_id").val(), {method: "POST"}).then(res => res.text().then(txt => $("#title").html(txt)))
 }
+
+
+function saveByteArray(reportName, byte) {
+    var blob = new Blob(byte, { type: "application/pdf" });
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    var fileName = reportName;
+    link.download = fileName;
+    link.click();
+};
 
 $(() => {
     $("#input_id").on('input', inputChanged)
